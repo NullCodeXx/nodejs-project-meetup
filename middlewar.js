a1f618ecfe5aa1bfa4cf8b60e4ea037e6c228d40
Djaafar Boumaza <26055564+djaafarcode@users.noreply.github.com>
2017-08-24 11:13:18 +0200

    first node MiddleWar not working

--- middlewar.js
+++ middlewar.js
@@ -1,19 +1,20 @@
 
 const express = require('express'); //npm install
-const body = require('body-parser'); //npm install
+const bodyParser = require('body-parser'); //npm install
 let app = express();
 
 app.use(express.static('public'));
-app.use( bodyParser.json() );
 
 //Enregistre un handler et associe une url à une function
-app.post("/auth",
+app.post(
+    "/auth",
     //Ajout d'un middlewar - Pour prendre en charge les corps codés par URL
-    app.use(bodyParser.urlencoded({ extended: true })),
+    bodyParser.urlencoded({ extended: true }),
     function(req, resp) {
     console.log(req.body);
     resp.send("Succès !");
-})
+    }
+);
 
 app.listen(80 , function() {
     if(err) {
